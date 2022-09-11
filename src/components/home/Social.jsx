a314ec8d474c0abcf9db6ea0a55b917118ccc1ca
Arjunvir Sidhu <asidhu17@uoguelph.ca>
2022-09-11 16:25:06 -0400

    navbar, home section completed

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./home.css"
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="https://github.com/rjnvr" className="home__social-icon" target="_blank">
+        <i class="uil uil-github"></i>
+      </a>
+      
+      <a href="https://www.linkedin.com/in/arjunvir-sidhu-7394b11a3/" className="home__social-icon" target="_blank">
+        <i class="uil uil-linkedin"></i>
+      </a>
+      
+      <a href="https://github.com/rjnvr" className="home__social-icon" target="_blank">
+        <i class="uil uil-github-alt"></i>
+      </a>
+
+    </div>
+  )
+}
+
+export default Social
